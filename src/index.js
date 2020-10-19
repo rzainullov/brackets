82d840d3580f865cfc508a3d70bd182b688b9b65
rzainullov <rashid.zainullov@gmail.com>
2020-10-20 01:41:55 +0400

    start task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length == 0 || str.length == 1){
+    return str.length == 0 ? true : false;
+    }
+  else{
+    let a = str.split('');
+    for(let i = 0; i < a.length; i++){
+      for(let j = 0; j < bracketConfig.length; j++){
+        for(let k = 1; k < 2; k++){
+            if(bracketConfig[j][1] == a[i] && bracketConfig[j][0] !== bracketConfig[j][1]  ){
+                 if(bracketConfig[j][0] == a[i-1]){
+                    a.splice(i - 1,2)
+                    let b = a.join('')
+                    return bruckets(b,bracketConfig);
+                   } else {
+                     return false
+                   }
+            } 
+            else if(bracketConfig[j][1] == a[i] && bracketConfig[j][0] == bracketConfig[j][1]) {
+              a.splice(i,2)
+              let b = a.join('')
+              return bruckets(b,bracketConfig);
+            } 
+        }
+             
+       }
+    }
+  }
 }
